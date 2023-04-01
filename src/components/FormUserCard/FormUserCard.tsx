69a5fe9efada5de84c87772b3c1cab1792782d2d
AKiroi <alexandr.kiroi@gmail.com>
2023-04-01 21:42:01 +0300

    feat: change FormUserCard to func component

--- src/components/FormUserCard/FormUserCard.tsx
+++ src/components/FormUserCard/FormUserCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { RegistrationForm } from '../../pages/FormPage/FormPage';
 import styles from './FormUserCard.module.scss';
 
@@ -6,28 +6,28 @@ type FormUserCardProp = {
   user: RegistrationForm;
 };
 
-export default class FormUserCard extends React.Component<FormUserCardProp> {
-  render() {
-    const { photo, firstName, lastName, birthday, country, sex } = this.props.user;
-    return (
-      <li className={styles.user} data-testid="user">
-        <img className={styles.photo} src={photo} alt="user photo" />
-        <div className={styles.col}>
-          <span className={styles.name}>First name:</span> {firstName}
-        </div>
-        <div className={styles.col}>
-          <span className={styles.name}>Last name:</span> {lastName}
-        </div>
-        <div className={styles.col}>
-          <span className={styles.name}>Birthday:</span> {birthday}
-        </div>
-        <div className={styles.col}>
-          <span className={styles.name}>Gender:</span> {sex}
-        </div>
-        <div className={styles.col}>
-          <span className={styles.name}>Country:</span> {country}
-        </div>
-      </li>
-    );
-  }
-}
+const FormUserCard: FC<FormUserCardProp> = ({ user }) => {
+  const { photo, firstName, lastName, birthday, country, sex } = user;
+  return (
+    <li className={styles.user} data-testid="user">
+      <img className={styles.photo} src={photo} alt="user photo" />
+      <div className={styles.col}>
+        <span className={styles.name}>First name:</span> {firstName}
+      </div>
+      <div className={styles.col}>
+        <span className={styles.name}>Last name:</span> {lastName}
+      </div>
+      <div className={styles.col}>
+        <span className={styles.name}>Birthday:</span> {birthday}
+      </div>
+      <div className={styles.col}>
+        <span className={styles.name}>Gender:</span> {sex}
+      </div>
+      <div className={styles.col}>
+        <span className={styles.name}>Country:</span> {country}
+      </div>
+    </li>
+  );
+};
+
+export default FormUserCard;
